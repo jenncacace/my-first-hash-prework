74801eb020628c8f609b5663fd5851051fa6e452
Jenn Cacace <jenn.cacace@gmail.com>
2018-01-16 19:04:46 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,12 +9,13 @@ end
 
 def retrieval
     shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-    shipping_manifest["whale bone corsets"]
+    shipping_manifest["oil paintings"]
 end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   shipping_manifest["pearl necklace"] = 1 
+  return shipping_manifest
 
 end
 
